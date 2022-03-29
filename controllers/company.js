4e8ebdeb306541434c4d6a598cf1820bf274bc91
erikqb3 <erikqb3@gmail.com>
2022-03-29 10:34:21 -0600

    add company controller code and npm install

---
+++ controllers/company.js
@@ -0,0 +1,68 @@
+exports.getAllCompanies = (req, res, next) => {
+  console.log(company.name);
+  User.find()
+  .then(company =>{
+      res.status(200).json({
+          company: company
+  });
+
+          // content: req.userId }]
+  });
+};
+
+exports.getCompany = (req, res, next) => {
+  console.log(req.params.id, "16");
+  company.findById(req.params.id)
+  .then(company =>{
+      console.log(company, "19");
+      res.status(200).json({
+          company: company._id
+  });
+
+          // content: req.userId }]
+  });
+};
+
+exports.getCompanyByUser = (req, res, next) => {
+  console.log(req.params.id, "16");
+  company.findById(req.params.id)
+  .then(company =>{
+      console.log(company, "19");
+      res.status(200).json({
+          company: company._id
+  });
+
+          // content: req.userId }]
+  });
+};
+
+exports.getCompanyByTag = (req, res, next) => {
+  console.log(req.params.id, "16");
+  company.findById(req.params.id)
+  .then(company =>{
+      console.log(company, "19");
+      res.status(200).json({
+          company: company._id
+  });
+
+          // content: req.userId }]
+  });
+};
+
+exports.postCompany = (req, res, next) => {
+  res.status(200).json({
+      posts: [{ title: 'company', content: 'This is the company endpoint' }]
+  });
+};
+
+exports.putCompany = (req, res, next) => {
+  res.status(200).json({
+      posts: [{ title: 'company', content: 'This is the company endpoint' }]
+  });
+};
+
+exports.deleteCompany = (req, res, next) => {
+  res.status(200).json({
+      posts: [{ title: 'company', content: 'This is the company endpoint' }]
+  });
+};
